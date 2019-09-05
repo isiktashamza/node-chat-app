a01859b79aff756e523e548cdd160d165116c457
isiktashamza <44121357+isiktashamza@users.noreply.github.com>
2019-09-05 14:36:07 +0300

    socket broadcast added

--- server/server.js
+++ server/server.js
@@ -22,6 +22,17 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from : 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
 
     socket.on('disconnect', () => {
         console.log('the user was disconnected');
@@ -29,7 +40,7 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
        console.log(message);
-       io.emit('newMessage', {
+       socket.broadcast.emit('newMessage', {
            from: message.from,
            text: message.text,
            createdAt: new Date().getTime()
